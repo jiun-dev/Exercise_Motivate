76aac1214dfb39353697f5246e77562704609166
jiun-dev <jiun.dev@gmail.com>
2021-05-26 20:24:46 +0900

    Initial Commit

---
+++ Motivation/MotivationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotivationApp.swift
+//  Motivation
+//
+//  Created by shinjiun on 2021/05/26.
+//
+
+import SwiftUI
+
+@main
+struct MotivationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
